2bb9684e208a4df4a16bf847bb5a8a601c1af29e
usman.shahid <usman.shahid@grp.contegris.com>
2026-02-01 02:03:40 +0500

    Boiler plate

--- index.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const app = require('./src/app');
-
-app.listen(process.env.PORT, () => {
-  console.log(`WhatsApp API running on port ${process.env.PORT}`);
-});
---
+++ server.js
@@ -0,0 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const app = require('./src/app');
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`WhatsApp API running on port ${PORT}`);
+});
