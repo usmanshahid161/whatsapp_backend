d6998492031c760c59b024817adca4257eec8245
usman.shahid <usman.shahid@grp.contegris.com>
2026-02-01 02:11:03 +0500

    Boiler plate

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.use('/webhook', require('./routes/webhook'));
 app.use('/messages', require('./routes/message'));
 
